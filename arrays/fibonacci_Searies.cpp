fbdca0e4de683b20bed6f72c965e2954c576d073
Gaurav Nayak <91482177+Gaurav2349@users.noreply.github.com>
2026-03-29 09:55:07 +0530

    Add Fibonacci series implementation in C++

---
+++ arrays/fibonacci_Searies.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int a=0;
+    int b=1;
+    int c;
+    int input=6;
+    
+    for(int i=0;i<input;i++){
+        cout<<a<<endl;
+        c=a+b;
+        a=b;
+        b=c;
+        
+        
+        
+    }
+    
+    return 0;
+    
+}
